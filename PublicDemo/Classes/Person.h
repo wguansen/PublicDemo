2035aac7cc3522df83207248cd9fa4f51f576295
admin <>
2019-09-05 14:30:57 +0800

    创建

---
+++ PublicDemo/Classes/Person.h
@@ -0,0 +1,19 @@
+//
+//  Person.h
+//  wang_Example
+//
+//  Created by admin on 2019/9/5.
+//  Copyright © 2019 wguansen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
++(void)person;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PublicDemo/Classes/Person.m
@@ -0,0 +1,18 @@
+//
+//  Person.m
+//  wang_Example
+//
+//  Created by admin on 2019/9/5.
+//  Copyright © 2019 wguansen. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
++(void)person
+{
+    NSLog(@"person");
+}
+
+@end
